41a62fcd4e3d8978e5a76c72c569122202a0c1ef
Mohamed <96751991+Rawdet@users.noreply.github.com>
2025-07-21 07:45:13 +0300

    Add files via upload

---
+++ language (4).js
@@ -0,0 +1,19 @@
+function changeLanguage(lang) {
+  const page = window.location.pathname.split("/").pop().split(".")[0];
+  if (lang === "ar") {
+    // يرجع للمجلد الأساسي
+    window.location.href = "/rawdat-alnoor/" + page + ".html";
+  } else {
+    // يدخل على النسخة الإنجليزية
+    window.location.href = "/rawdat-alnoor/en/" + page + ".html";
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const select = document.querySelector('select');
+  if (window.location.pathname.includes('/en/')) {
+    select.value = 'en';
+  } else {
+    select.value = 'ar';
+  }
+});
